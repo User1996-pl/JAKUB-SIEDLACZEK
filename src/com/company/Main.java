4a972c609b46ff051d5370ae0668e40a7eba3778
unknown <Admin@JAKUBSIEDLACZEK.pks.bielsko>
2019-12-07 12:43:45 +0100

    string

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String imie, nazwisko;
+        int wiek;
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Wporowadź Imie: ");
+        imie = scanner.next();
+
+        System.out.println("Wporowadź nazwisko: ");
+        nazwisko = scanner.next();
+
+        System.out.println("Wporowadź wiek: ");
+        wiek = scanner.nextInt();
+
+        System.out.println("-----Wprowadzone dane-----");
+
+
+
+
     }
 }
